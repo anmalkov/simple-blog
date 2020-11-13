7b587e1a586d65f010dc30476aa65e4abaafacca
Andrew Malkov <1496507+anmalkov@users.noreply.github.com>
2020-11-13 17:12:21 +0100

    Migrated to dotnet 5 (#2)
    Co-authored-by: Andrew Malkov <anmalkov@microsoft.com>

--- scr/SimpleBlog/Startup.cs
+++ scr/SimpleBlog/Startup.cs
@@ -28,7 +28,7 @@ namespace SimpleBlog
             services.AddSingleton<IArticlesRepository, DiskArticlesRepository>();
             services.AddScoped<IArticlesService, ArticlesService>();
 
-            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
