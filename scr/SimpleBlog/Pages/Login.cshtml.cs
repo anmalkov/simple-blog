00a1a20e8d7f01a4e0b7b3c0ad0fb1fe30028784
Andrew Malkov <anmalkov@microsoft.com>
2020-11-21 07:34:34 +0100

    Login page handlers

--- scr/SimpleBlog/Pages/Login.cshtml.cs
+++ scr/SimpleBlog/Pages/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +10,29 @@ namespace SimpleBlog.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        [Required]
+        [Display(Name = "Enter your code")]
         public string Code { get; set; }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/admin");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/admin");
         }
     }
 }
