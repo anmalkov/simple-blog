b26d0eed1dc77a36deaa373e11690c02885df46a
Andrew Malkov <anmalkov@microsoft.com>
2020-11-21 07:23:25 +0100

    Add login page

---
+++ scr/SimpleBlog/Pages/Login.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleBlog.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public string Code { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
