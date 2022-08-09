5785c4021cbdce5c243c4ae7b32a00a51d032c44
anmalkov <an.malkov@outlook.com>
2022-08-09 13:39:53 +0200

    Copy Title to URL when creating a new page

--- src/SimpleBlog/wwwroot/js/site.js
+++ src/SimpleBlog/wwwroot/js/site.js
@@ -2,3 +2,8 @@
     var element = document.getElementById(textareaId);
     element.value = "[" + fileName + "]: /blogimages/" + folderName + "/" + fileNameWithExt + " \"" + fileName + "\"\r\n" + element.value;
 }
+function titleToUrl(titleInputId, urlInputId) {
+    var title = document.getElementById(titleInputId).value;
+    var url = title.toLowerCase().replace(/ /g, "-");
+    document.getElementById(urlInputId).value = url;
+}
